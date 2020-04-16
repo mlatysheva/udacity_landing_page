bd1e922ec3a6a313e50c25e7667809caab949446
Maria Latysheva <mlatysheva@Marias-MBP.fritz.box>
2020-04-16 22:33:54 +0200

    Final version

--- README.md
+++ README.md
@@ -2,12 +2,33 @@
 
 ## Table of Contents
 
-* [Instructions](#instructions)
+* Purpose of the Project
+* Tasks of the Project
+* Implementation procedures
 
-## Instructions
+### Purpose of the Project 
+The purpose of the Project is to show some basic capabilities of javascript using some of the most widely used features.
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+### Tasks of the Project
 
-To get started, open `js/app.js` and start building out the app's functionality
+There are three tasks the Project is solving:
+1) to build dynamically a navigation menu with javascript tools
+2) to check with section of the page is currently in view and to highlight this section 
+by applying the relevant CSS properties
+3) to smoothly scroll to the section that has been clicked on the navigation bar
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+### Implementation procedures
+
+1) Building a dymanically generated navigation menu
+In this task we build a navigation menu by creating li-elements containg relevant features from the sections attributes and 
+subsequently appending them to their parent ul element.
+
+2) Listening to a click event on the navigation bar to highlight the section shosen
+In this task we listen to a click event on the navigation bar. We take the target of the click event and choose the property #hash, removing the fist element from the #hash string to get the id of the section clicked.
+Then we change the CSS style of the active section, having removed any active status from other sections
+
+3) Scrolling smothly to the section clicked on the navigation bar
+In this task we listen to a click event by iterating through all anchored links on the page.
+Once the anchor link is clicked, we scroll smoothly to the section chosen.
+
+All these features are usable across all modern browsers.
--- js/app.js
+++ js/app.js
@@ -18,62 +18,51 @@
  * Define Global Variables
  * 
 */
-let sectionList = document.getElementsByTagName("section");
+let sectionList = document.getElementsByTagName('section');
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+// Build the dynamic navigation menu
 
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-const ul = document.getElementById("navbar__list");
+const ul = document.getElementById('navbar__list');
 for (let i = 0; i < sectionList.length; i++) {
-    let navSectionName = sectionList[i].getAttribute("data-nav"); // store the menu item name, for example "Section 1"
-    let navID = sectionList[i].getAttribute("id");  // store the id of the menu item, for example "section1"
-    let liItem = document.createElement("li"); // create the new list element li
-    liItem.classList.add("navbar__menu"); // add the class value to the newly created li element
-    liItem.innerHTML = `<a class = "menu__link" href='#${navID}'>${navSectionName}</a>`; // populate the li element with its HTML code
+    const navSectionName = sectionList[i].getAttribute('data-nav'); // store the menu item name, for example "Section 1"
+    const navID = sectionList[i].getAttribute('id');  // store the id of the menu item, for example "section1"
+    const liItem = document.createElement('li'); // create the new list element li
+    liItem.classList.add('navbar__menu'); // add the class value to the newly created li element
+    liItem.innerHTML = `<a class = 'menu__link' href='#${navID}'>${navSectionName}</a>`; // populate the li element with its HTML code
     ul.appendChild(liItem); // append the newly created li element to its parent element ul
     
 };
 
-// remove the active class from the acti
+// Remove the active class if was active and set to active for the currently viewed section
+// by iterating through the sectionList elements
+
 function changeToActive(parameter) {
     for (let j = 0; j < sectionList.length; j++) {
-        sectionList[j].classList.remove("your-active-class");
+        sectionList[j].classList.remove('your-active-class'); // first remove the active class from all sections
     }
-    document.getElementById(parameter).classList.add("your-active-class"); 
+    document.getElementById(parameter).classList.add('your-active-class');  // set the active class for the currently viewed section
 }
 
-document.getElementById("navbar__list").addEventListener("click", function(e) {
-    changeToActive(e.target.hash.substring(1));
+// Set the event listener to a click on the navigation bar in order to change the active status of the respective section
+
+document.getElementById('navbar__list').addEventListener('click', function(e) {
+    changeToActive(e.target.hash.substring(1)); // change the status for the section based on its id parameter
 });
 
 
 
 
-// Scroll to anchor ID using scrollTO event
+// Scroll smoothly to a section on a link click
 
-  
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {   // select all ancors on the page to iterate over them
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault(); // prevent the default action of following a link
+        document.querySelector(this.getAttribute('href')).scrollIntoView({  // get the clicked section into view by scrolling to it
+            behavior: 'smooth'
+        });
+    });
+});
 
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
 
 
 
